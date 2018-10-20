55cbc78ea4fc15d0704008579bcb46c0dd42fbc9
Isaac Curtiss <ijcurtiss7@gmail.com>
2018-10-20 17:35:37 -0400

    fixed mapping

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import projects from '../data/projects.json';
 import React from 'react';
-
+import Project from './Project'
 
 class Projects extends React.Component {
   render() {
@@ -16,24 +16,11 @@ class Projects extends React.Component {
             <p>These are some of my projects</p>
         </header>
         <div id="projects">
-            <h2>Project 1</h2>
-            <div className="portfolio-img iron-man" title="Iron-man"></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div id="projects">
-          <h2>Project 2</h2>
-          <div className="portfolio-img phoenix"></div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div id="projects">
-            <h2>Project 3</h2>
-            <div className="portfolio-img storm"></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div title="Project 4" id="projects">
-            <h2>Project 4</h2>
-            <div className="portfolio-img gambit"></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            {projects.allProjects.map((e, i) => {
+              return (
+                <Project title={e.title} description={e.description} imgName={e.imgName} key={i} />
+              )
+            })}
         </div>
       </section>
 
